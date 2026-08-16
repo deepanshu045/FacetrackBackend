b6560b941db39934d64d94613698556de14f2789
Deepanshu Vishwakarma <deepanshukarma7@gmail.com>
2026-08-16 16:04:56 +0530

    Register class section and teacher models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,7 +3,10 @@ from app.models.admin import Admin
 from app.models.student import Student
 from app.models.attendance import Attendance
 from app.models.absence_notification import AbsenceNotification
+from app.models.attendance_summary_notification import AttendanceSummaryNotification
 from app.models.pending_college_registration import PendingCollegeRegistration
 from app.models.lecture import Lecture
 from app.models.lecture_schedule import LectureSchedule
 from app.models.college_closure import CollegeClosure
+from app.models.class_section import ClassSection
+from app.models.teacher import Teacher, TeacherAssignment
