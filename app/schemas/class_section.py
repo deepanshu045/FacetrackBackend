7dbbe1e68f573cefe328b6d62a1169b6f7b06dfe
Deepanshu Vishwakarma <deepanshukarma7@gmail.com>
2026-09-20 11:57:40 +0530

    Support flexible academic year values

--- app/schemas/class_section.py
+++ app/schemas/class_section.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel, ConfigDict, field_validator
 
 
+CLASS_YEAR_MAP = {
+    "FY": "FY", "SY": "SY", "TY": "TY",
+    "4Y": "4Y", "5Y": "5Y", "6Y": "6Y", "7Y": "7Y", "8Y": "8Y", "9Y": "9Y", "10Y": "10Y",
+    "FIRST YEAR": "FY", "SECOND YEAR": "SY", "THIRD YEAR": "TY", "FOURTH YEAR": "4Y",
+    "FIFTH YEAR": "5Y", "SIXTH YEAR": "6Y", "SEVENTH YEAR": "7Y", "EIGHTH YEAR": "8Y",
+    "NINTH YEAR": "9Y", "TENTH YEAR": "10Y",
+    "1ST YEAR": "FY", "2ND YEAR": "SY", "3RD YEAR": "TY", "4TH YEAR": "4Y",
+    "5TH YEAR": "5Y", "6TH YEAR": "6Y", "7TH YEAR": "7Y", "8TH YEAR": "8Y",
+    "9TH YEAR": "9Y", "10TH YEAR": "10Y",
+}
+
+
 class ClassSectionCreate(BaseModel):
     department: str
     class_name: str
@@ -15,18 +27,11 @@ class ClassSectionCreate(BaseModel):
         if info.field_name == "department" and value.upper() == "BCA":
             return "BSc CS"
         if info.field_name == "class_name":
-            class_map = {
-                "FY": "FY",
-                "SY": "SY",
-                "TY": "TY",
-                "FIRST YEAR": "FY",
-                "SECOND YEAR": "SY",
-                "THIRD YEAR": "TY",
-                "1ST YEAR": "FY",
-                "2ND YEAR": "SY",
-                "3RD YEAR": "TY",
-            }
-            return class_map.get(value.upper(), value)
+            normalized = CLASS_YEAR_MAP.get(value.upper())
+            if normalized:
+                return normalized
+            if value.upper().endswith("Y") and value[:-1].isdigit() and 1 <= int(value[:-1]) <= 99:
+                return value[:-1] + "Y"
         return value
 
 
